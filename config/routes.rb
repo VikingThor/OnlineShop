e982d4b6902a217f9fbde48938a2bc77a4c4fd67
VikingThor <thorstenzoellner@gmail.com>
2019-09-18 09:59:53 +0200

    basic functions onlineshop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  get 'carts/show'
+  resources :products
+
+  resources :shops, only:[:index, :show]
+  resources :order_items
+  resource :carts, only:[:show]
+
+
+
+  root 'shops#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
